809f251ca03211352f69b9c56488d301e4b499b3
Rohit <srohit1101@gmail.com>
2020-05-20 17:03:06 +0530

    not refactored

--- src/Todo.js
+++ src/Todo.js
@@ -6,10 +6,17 @@ export class Todo extends React.Component {
   state = {
     todo: "",
     todo_arr: [],
+    edit: false,
+    editID: "",
+    editedText: "",
   };
 
   handleChange = (e) => {
     this.setState({ todo: e.target.value });
+  };
+
+  handleEditChange = (e) => {
+    this.setState({ editedText: e.target.value });
   };
 
   handleClick = () => {
@@ -30,43 +37,68 @@ export class Todo extends React.Component {
     // const index = this.state.todo_arr.findIndex((todo) => {
     //   return todo.id === id;
     // });
-
     // this.setState((state) => ({ todo_arr: state.todo_arr.splice(index, 1) }));
-
     // console.log(index);
     const newA = this.state.todo_arr.filter((todo) => {
       return todo.id !== id;
     });
-
-    console.log(newA);
-
     this.setState({ todo_arr: newA });
   };
 
-  edit = (id) => {
+  edit = (item, id) => {
+    this.setState({ edit: !this.state.edit });
+    this.setState({ editedText: item, editID: id });
+    // this.setState((state) => {
+    //   return state.todo_arr.map((item) => {
+    //     return item.id === id ? (item.todo: editedText) : "";
+    //   });
+    // });
+  };
+
+  handleBlur = (e) => {
+    console.log(e.target.value);
+    // this.setState((state) => {
+    //   return state.todo_arr.map((item) => {
+    //     return item.id === state.editId && (item.todo = state.editedText);
+    //   });
+    // });
+  };
+
+  handleUpdate = (e) => {
     this.setState((state) => {
       return state.todo_arr.map((item) => {
-        return item.id === id && (item.todo = "done");
+        return item.id === state.editID && (item.todo = state.editedText);
       });
     });
+    this.setState({ edit: false });
   };
 
   render() {
     return (
       <div className="App">
         <h1>TODO</h1>
+        {this.state.edit ? (
+          <div>
+            <input
+              value={this.state.editedText}
+              onChange={this.handleEditChange}
+              onInput={this.handleBlur}
+            />
+            <button onClick={this.handleUpdate}>Update</button>
+          </div>
+        ) : (
+          ""
+        )}
         <input
-          value={this.state.todo}
+          value={this.state.edit ? "" : this.state.todo}
           placeholder="Search"
           onChange={this.handleChange}
         />
         <button onClick={this.handleClick}>Add Todo</button>
         <TodoItems
-          sot={this.state}
-          edit={this.edit}
           todo={this.state.todo_arr}
           del={this.handleDelete}
-          newInp={this.handleChange}
+          editHandler={this.edit}
         />
       </div>
     );
--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,17 +1,25 @@
 import React from "react";
 
-export function TodoItems({ todo, del, edit, sot, newInp }) {
+export function TodoItems({ todo, del, editHandler }) {
   let i = 0;
   return (
     <div>
       <div>
         {todo.map((item) => {
           return (
-            <p key={i++} id={item.id}>
-              {item.todo} created at {new Date(item.id).toLocaleTimeString()}
-              <i className="fas fa-edit" onClick={(e) => edit(item.id)} />
-              <i className="fas fa-trash-alt" onClick={(e) => del(item.id)} />
-            </p>
+            <div key={i++}>
+              <p id={item.id}>
+                {item.todo} created at {new Date(item.id).toLocaleTimeString()}
+                <i
+                  className="fas fa-edit"
+                  onClick={(e) => editHandler(item.todo, item.id)}
+                />
+                <i
+                  className="fas fa-trash-alt"
+                  onClick={(e) => del(item.id, item.todo)}
+                />
+              </p>
+            </div>
           );
         })}
       </div>
